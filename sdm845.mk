edfc1f2c04d2b5176d789353bc39d23095162b3b
Cruzer1 <preetamswain143@gmail.com>
2021-07-21 03:23:14 +0000

    livedisplay: Add Specified target

--- sdm845.mk
+++ sdm845.mk
@@ -232,7 +232,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_sdm845
 
 # Media
 PRODUCT_PACKAGES += \
